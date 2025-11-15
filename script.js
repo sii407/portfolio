6552187b6921270ece2d0dfe66fae4d70c500db4
sii_407 <sii407@users.noreply.github.com>
2025-11-15 16:22:51 +0900

    change hamburger-menu

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("hamburger-menu");
+
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
+});
+
+const links = menu.querySelectorAll("a");
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
 });
